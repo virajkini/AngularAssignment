c608522913866c2d482003322a1a64480213b657
virajkini <kini_viraj2006@yahoo.co.in>
2017-03-15 14:09:33 +0530

    Add files via upload

---
+++ Library.js
@@ -0,0 +1,60 @@
+var app = angular.module("mylibrary",['ngRoute']);
+
+app.config(function($routeProvider) {
+    $routeProvider
+
+        .when("/", {
+            templateUrl : "home.html"
+        })
+
+        .when("/add", {
+            templateUrl : "video_lists.html",
+            controller : "videocontroller"
+
+
+        });
+
+
+});
+
+
+
+app.controller("videocontroller",["$scope",function($scope) {
+
+
+    var video_count = 0;
+    $scope.myLibrary = [];
+
+    $scope.add = function()
+    {
+
+
+        var getId = function (url) {
+
+            var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+            var match = url.match(regExp);
+
+            if (match && match[2].length == 11) {
+                return match[2];
+            } else {
+                return 'error';
+            }
+        }
+
+
+        var str="https://www.youtube.com/watch?v=EpHqeHF8NM0&t=1s";
+
+        var myId = getId(str);
+
+        var myCode = '"https://www.youtube.com/embed/'+ myId + '"';
+
+
+
+
+alert(myCode)
+
+        $scope.myLibrary[video_count] = myCode;
+        video_count++;
+        console.log($scope.myLibrary[0])
+    }
+}])
---
+++ videocontroller.js
@@ -0,0 +1,4 @@
+app.controller("videocontroller",["$http",function($http) {
+
+
+}])
